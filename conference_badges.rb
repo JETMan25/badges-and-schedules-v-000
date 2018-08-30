ea1ca55a127bd03a215cd0ad4a8f72b660903c81
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-30 16:00:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,10 +20,10 @@ end
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
-    
+  end
     assign_rooms(attendees).each do |assignment|
       puts assignment
-    end
+  
   end
  end
 # Write your code here.
